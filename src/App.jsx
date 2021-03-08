ea11613fac5a5ebc35e0011e7cbb31f1b84686ac
Manav Misra <manavm@visionwebsoft.com>
2021-03-08 17:48:07 -0600

    Update movies state from TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { Home } from "./pages";
 
-// (async () => {
-//   console.log(await api.index());
-// })();
-
 function App() {
   // TODO: Use React Router to render the different pages
   return <Home />;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
+import api from "api";
 import { Main } from "components";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  // Runs on every render (in response side effect)
+  useEffect(
+    () => {
+      (async () => {
+        const { results } = await api.index();
+        setMovies(() => results);
+      })();
+    },
+
+    // Empty dep array means on initial render
+    []
+  );
+
   return (
     <Main>
       <p>Hello World</p>
