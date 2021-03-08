133d31ce3dadc1fc3f3264fa06c5f420c0a61025
Manav Misra <manavm@visionwebsoft.com>
2021-03-08 17:18:38 -0600

    :construction: Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,29 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
-const Card = ({ title, id }) => {
-  return <Link to={`/${id}`}>{title}</Link>;
+const Card = ({ title, path, date, rating }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <figure>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${path}`}
+          alt={`Movie poster for ${title}`}
+        />
+        <figcaption>
+          {date} - {rating}
+        </figcaption>
+      </figure>
+      {/* TODO: Use `<Link to={`/{id}`}/> */}
+      <button>More Info!</button>
+    </section>
+  );
 };
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Card;
