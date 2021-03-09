2d9486f0a199ffe9593ad2014f21fb50c7e9daf5
Manav Misra <manavm@visionwebsoft.com>
2021-03-09 15:06:28 -0600

    :construction: Film page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { Home } from "./pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Film, Home } from "./pages";
 
 function App() {
-  // TODO: Use React Router to render the different pages
-  return <Home />;
+  return (
+    <Router>
+      {/* TODO: Set up the Route and Switch such that Home is rendered '/' and 'Film' is rendered when we go to '/Film' */}
+      <Switch>
+        <Route path="/film">
+          <Film />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
