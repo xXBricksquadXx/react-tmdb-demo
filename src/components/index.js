9f98618bcb6b39449ec61efa50da8641f6d75ca4
Manav Misra <manavm@visionwebsoft.com>
2021-03-09 13:09:48 -0600

    Ren Card to MovieCard

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Card } from "./Card";
 export { default as Main } from "./Main";
+export { default as Card } from "./MovieCard";
 export { default as Search } from "./SearchBar";
