a5a569a6e4ad083714356c90ec04b7f073fe6874
Manav Misra <manavm@visionwebsoft.com>
2021-03-10 17:06:55 -0600

    Add handler prop

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -1,19 +1,24 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-const Button = ({ txt }) => {
+const Button = ({ txt, handler }) => {
   return (
-    <button className="transition duration-500 ease-in-out bg-green-700 hover:bg-green-500 transform text-white rounded p-2 hover:-translate-y-1 hover:scale-110">
+    <button
+      className="transition duration-500 ease-in-out bg-green-700 hover:bg-green-500 transform text-white rounded p-2 hover:-translate-y-1 hover:scale-110"
+      onClick={handler}
+    >
       {txt}
     </button>
   );
 };
 
 Button.propTypes = {
+  handler: PropTypes.func,
   txt: PropTypes.string,
 };
 
 Button.defaultProps = {
+  handler: () => {},
   txt: "Submit",
 };
 
