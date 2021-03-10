8923fd4efb7cee65e504010c4510ca4b65e31d77
Manav Misra <manavm@visionwebsoft.com>
2021-03-10 16:58:26 -0600

    :seedling: MovieModal

---
+++ src/components/MovieModal/index.jsx
@@ -0,0 +1,36 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import PropTypes from "prop-types";
+
+function MovieModal({ title, overview }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{overview}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+MovieModal.propTypes = {
+  title: PropTypes.string.isRequired,
+  overview: PropTypes.string,
+};
+
+MovieModal.defaultProps = {
+  overview: "No overview provided",
+};
+
+export default MovieModal;
